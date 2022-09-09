136dae883189fde3d71479720b401c333309cbf0
«alexander <favorite@bk.ru>
2022-09-09 21:19:16 +0300

    Les

---
+++ Les_2/Program.cs
@@ -0,0 +1,31 @@
+﻿int[,] GetArray(int m, int n) {
+  int[,] result = new int[m, n];
+
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      result[i, j] = i + j;
+    }
+  }
+
+  return result;
+}
+
+void PrintArray(int[,] array) {
+  for (int i = 0; i < array.GetLength(0); ++i) {
+    for (int j = 0; j < array.GetLength(1); ++j) {
+      Console.Write(array[i, j]);
+    }
+    Console.WriteLine();
+  }
+}
+
+Console.WriteLine(" Vvedtite kolvo strok");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine(" Vvedtite kolvo stolbcov");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+

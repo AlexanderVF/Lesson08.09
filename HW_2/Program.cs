ff3d734722960cc9f0acb6560cdfb7d4bf25d04e
«alexander <favorite@bk.ru>
2022-09-11 09:43:39 +0300

    HW

---
+++ HW_2/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine(" Vvedtite chislo");
+int d = int.Parse(Console.ReadLine());
+int c = 0;
+
+int[,] GetArray(int m, int n) {
+  int[,] result = new int[m, n];
+
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      result[i, j] = new Random().Next(1, 100);
+    }
+  }
+
+  return result;
+}
+
+void PrintArray(int[,] array) {
+  for (int i = 0; i < array.GetLength(0); ++i) {
+    for (int j = 0; j < array.GetLength(1); ++j) {
+      Console.Write($" {array[i, j]}");
+    }
+    Console.WriteLine();
+        
+    }
+}
+
+Console.WriteLine(" Vvedtite kolvo strok");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine(" Vvedtite kolvo stolbcov");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+
+for (int i = 0; i < array.GetLength(0); ++i) 
+{
+    for (int j = 0; j < array.GetLength(1); ++j)
+    {
+        if (array[i,j] != d)
+        {
+        c = array[i,j];
+        }
+    }
+}
+
+Console.WriteLine();
+
+if (c != d)
+{
+Console.WriteLine(d + " -> takogo chisla v massive net");
+}
+else
+{
+    Console.WriteLine(d + " -> takoe chislo v massive est'");
+}
---
+++ HW_3/Program.cs
@@ -0,0 +1,46 @@
+﻿
+int[,] GetArray(int m, int n)
+{
+    int[,] a = new int[m, n];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            a[i,j] = new Random().Next(1, 10);
+        }
+    }
+    return a;
+}
+
+void PrintArray (int[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine(" Vvedtite kolvo strok");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine(" Vvedtite kolvo stolbcov");
+int columns = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+Console.WriteLine();
+
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+for (int j = 0; j < array.GetLength(1); j++) {
+    double sum = 0;
+    int n = 0;
+    for (int i = 0; i < array.GetLength(0); i++) {
+        sum = array[i, j] + sum;
+        n++;
+    }
+        Console.Write(Math.Round((sum / n), 2) + ", ");
+      
+}
+Console.WriteLine();
+Console.WriteLine();
